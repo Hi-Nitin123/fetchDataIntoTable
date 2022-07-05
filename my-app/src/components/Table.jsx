b65a92d0a0553b22ddf3828466f039123f7a141c
Nitin Pandey <npandey@deqode.com>
2022-07-05 14:12:59 +0530

    Create a table and also do styling of it

---
+++ my-app/src/components/Table.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Table.css";
+  return (
+    <div>
+      <table className="table">
+        <tbody>
+          <tr>
+            <th>AlbumId</th>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Url</th>
+            <th>Thumbnailurl</th>
+          </tr>
+export default Table;

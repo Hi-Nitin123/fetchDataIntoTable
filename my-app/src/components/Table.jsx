d2ea7caab76f24658e7abc2867af7122137186c2
Nitin Pandey <npandey@deqode.com>
2022-07-05 14:14:15 +0530

    Fetch data from an api and insert it in the table

--- my-app/src/components/Table.jsx
+++ my-app/src/components/Table.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Table.css";
+import axios from "axios";
+
+function Table() {
+  const [post, setPost] = useState([]);
+  //   const [count, setCount] = useState(1);
+  useEffect(() => {
+    async function getData() {
+      const data = await axios
+        .get("https://jsonplaceholder.typicode.com/photos")
+        .then((res) => {
+          setPost(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      console.log("type:", typeof data);
+    }
+
+    getData();
+  }, []);
   return (
     <div>
       <table className="table">
@@ -11,4 +32,22 @@ import "../css/Table.css";
             <th>Url</th>
             <th>Thumbnailurl</th>
           </tr>
+          {console.log("post:", post)}
+          {post.map((data) => {
+            return (
+              <tr key={data.id}>
+                <td>{data.albumId}</td>
+                <td>{data.id}</td>
+                <td>{data.title}</td>
+                <td className="blueTd">{data.url}</td>
+                <td className="turl">{data.thumbnailUrl}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
 export default Table;
